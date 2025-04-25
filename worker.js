cbf1b032d5067f8449cfac1651809c8a1b8e92a6
Meszaros.David1 <161326964+Meszdavid@users.noreply.github.com>
2025-04-25 20:43:53 +0200

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,26 @@
-onmessage = function () {
-    let sum = 0;
-    for (let i = 0; i < 1e8; i++) {
-        sum += i;
+onmessage = function (e) {
+  const limit = 1_000_000;
+
+  const sieve = new Uint8Array(limit + 1);
+  sieve.fill(1);
+  sieve[0] = 0;
+  sieve[1] = 0;
+  
+
+  for (let i = 2; i * i <= limit; i++) {
+    if (sieve[i]) {
+      for (let j = i * i; j <= limit; j += i) {
+        sieve[j] = 0;
+      }
     }
-    postMessage("Összeg: " + sum);
+  }
+  
+
+  let sum = 0;
+  for (let i = 2; i <= limit; i++) {
+    if (sieve[i]) sum += i;
+  }
+  
+
+  postMessage(sum);
 };
